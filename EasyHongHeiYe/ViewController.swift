f8708542adf255da07bb02d7e922924fb9d61b45
howie.zhang <howie.zhang@alo7.com>
2018-11-28 17:01:13 +0800

    Initial Commit

---
+++ EasyHongHeiYe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyHongHeiYe
+//
+//  Created by 张和悦 on 2018/11/28.
+//  Copyright © 2018 张和悦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
